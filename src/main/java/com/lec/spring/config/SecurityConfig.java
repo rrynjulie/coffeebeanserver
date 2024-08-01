3014c1b35acf9c712bc424a5f9d965d6ed255a7c
jeongjunyeong <nejunyoung@gmail.com>
2024-08-01 19:45:24 +0900

    정준영_20240801_login_ver001

---
+++ src/main/java/com/lec/spring/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.lec.spring.config;
+
+// 스프링 시큐리티를 사용하기 위해서는
+// 스프링 시큐리티에 필요한 bean을 추가해 주는
+// config 클래스 파일을 추가해 줘야 합니다.
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // bean으로 생성해 준다
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // 패스워드 암호화를 위한 BCryptPasswordEncoder 를 빈에 추가
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 다 필요없는 auth는 비활시켜준다.
+        // csrf disable
+        http.formLogin((auth) -> auth.disable());
+
+        // Form 로그인 disable
+        http.httpBasic((auth) -> auth.disable());
+
+        // http baisc 인증방식 disable
+        http.httpBasic((auth) -> auth.disable());
+
+        // 경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/admin").hasRole("ADMIN") //  /admin 경로는 ADMIN 권한이 있을 때만 가능하다
+                        .requestMatchers("/user").hasAnyRole("USER", "ADMIN") // /member 경로는 멤버나 어드민 둘중에 하나만 있어도 가능하다
+                        .anyRequest().permitAll() // 나머지는 다 프리패스
+                );
+
+        // 세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션 사용 안함.
+
+
+        return http.build();
+    }
+}
--- src/main/java/com/lec/spring/controller/HomeController.java
+++ src/main/java/com/lec/spring/controller/HomeController.java
@@ -17,8 +17,12 @@ public class HomeController {
     // TODO
     // 홈 컨트롤러에 추가 하시고 싶으신 분들은 여기에 추가로 작성 요망
 
+    // 현재 Authentication 보기
     @RequestMapping("/auth")
     public Authentication auth() {
         return SecurityContextHolder.getContext().getAuthentication();
+        // SecurityContextHolder 안에 있는 getAuthentication 꺼내오기
+        // Authentication 이 인증된 정보이기 때문에 SecurityContextHolder 가 가지고 있는 값을 통해 인증이 되었는지 확인할 수 잇다.
+
     }
 }
