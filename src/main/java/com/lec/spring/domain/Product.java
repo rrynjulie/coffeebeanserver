61f7b1506e10990a06586edf03d2abd9d1bd5077
JaeHyeokee <jaehyeok817@naver.com>
2024-08-02 12:33:22 +0900

    이재혁_20240802_product domain 작성

--- src/main/java/com/lec/spring/domain/Product.java
+++ src/main/java/com/lec/spring/domain/Product.java
@@ -1,19 +1,68 @@
 package com.lec.spring.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.lec.spring.domain.enums.DealingStatus;
+import com.lec.spring.domain.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long productId;
+    private Long productId;  // 상품 ID
+    @ManyToOne(optional = false)
+    @ToString.Exclude
+    private User user;  // 판매자
+    private String name;  // 상품 이름
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String discription;  // 상품 설명
+    @Column(nullable = false)
+    private int price;  // 상품 가격
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(nullable = false)
+    private LocalDateTime regDate;  // 등록 날짜
+    @ColumnDefault(value = "'판매중'")
+    @Column(nullable = false)
+    private DealingStatus dealingStatus;  // 판매 상태
+    @Column(nullable = false)
+    private String category1;  // 카테코리1(패션의류, 모바일/태블릿, 가구/인테리어, 반려동물/취미, 티켓/쿠폰)
+    @Column(nullable = false)
+    private String category2;  // 카테코리2(여성의류, 남성의류, 스마트폰, 태블릿PC, 케이스/거치대/보호필름, 배터리/충전기/케이블, 침실가구, 거실가구, 주방가구, 기타가구, 반려동물, 키덜트, 핸드메이드/DIY, 악기, 티켓, 상품권/쿠폰)
+    @Column
+    private String category3;  // 카테코리3
+    @ColumnDefault(value = "'중고'")
+    @Column(nullable = false)
+    private Status status;  // 상품 상태
+    @Column(nullable = false)
+    private String dealingType;  // 거래 방식 (직거래, 택배)
+    @Column
+    private String desiredArea;  // 거래 희망 지역
+    @ColumnDefault(value = "0")
+    @Column(nullable = false, insertable = false)
+    private int viewCount;  // 조회수
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "attachment_id")
+    @ToString.Exclude
+    @Builder.Default   // builder 제공 안 함
+    private List<Attachment> fileList = new ArrayList<>();  // 첨부 파일
+
+    @PrePersist
+    protected void onCreate() {  // 등록 날짜 초기화
+        this.regDate = LocalDateTime.now();
+    }
+
+    public void addFiles(Attachment... files) {  // 첨부파일 추가
+        Collections.addAll(fileList, files);
+    }
 }
