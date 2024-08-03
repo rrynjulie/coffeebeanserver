ea9c0438fa02a3a8393052dfc7a93f7bec517053
JaeHyeokee <jaehyeok817@naver.com>
2024-08-03 18:18:53 +0900

    이재혁_20240803_상품테이블 작성

--- src/main/java/com/lec/spring/controller/ProductController.java
+++ src/main/java/com/lec/spring/controller/ProductController.java
@@ -9,35 +9,31 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin
 public class ProductController {
     private final ProductService productService;
 
     // 기본적인 CRUD
-    @CrossOrigin
     @PostMapping("/product/write")
     public ResponseEntity<?> create(@RequestBody Product product) {
         return new ResponseEntity<>(productService.create(product), HttpStatus.CREATED);
     }
 
-    @CrossOrigin
     @GetMapping("/product/list")
     public ResponseEntity<?> readAll() {
         return new ResponseEntity<>(productService.readAll(), HttpStatus.OK);
     }
 
-    @CrossOrigin
     @GetMapping("/product/detail/{productId}")
     public ResponseEntity<?> readOne(@PathVariable Long productId) {
         return new ResponseEntity<>(productService.readOne(productId), HttpStatus.OK);
     }
 
-    @CrossOrigin
     @PutMapping("/product/update")
     public ResponseEntity<?> update(@RequestBody Product product) {
         return new ResponseEntity<>(productService.update(product), HttpStatus.OK);
     }
 
-    @CrossOrigin
     @DeleteMapping("/product/delete/{productId}")
     public ResponseEntity<?> delete(@PathVariable Long productId) {
         return new ResponseEntity<>(productService.delete(productId), HttpStatus.OK);
--- src/main/java/com/lec/spring/domain/Attachment.java
+++ src/main/java/com/lec/spring/domain/Attachment.java
@@ -1,19 +1,30 @@
 package com.lec.spring.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity(name = "attachment")
 public class Attachment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long attachmentId;
+    private Long attachmentId;  // 첨부물 ID
+    @Column(name = "postId")
+    private Long postId;  // 게시글 ID
+    @Column(name = "productId")
+    private Long productId;  // 상품 ID
+    @Column(name = "carId")
+    private Long carId;  // 중고차 ID
+    @Column(name = "propertyId")
+    private Long propertyId;  // 부동산 ID
+    @Column(nullable = false)
+    private String source;  // 소스
+    @Column(nullable = false)
+    private String filename;  // 파일 이름
 }
