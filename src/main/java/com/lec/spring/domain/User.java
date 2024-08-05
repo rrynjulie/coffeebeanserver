9e88d468b5e3822002aecb4c3c1b7cb19186aa69
jeongjunyeong <nejunyoung@gmail.com>
2024-08-05 11:28:29 +0900

    정준영_20240805_User_domain_수정

--- src/main/java/com/lec/spring/domain/User.java
+++ src/main/java/com/lec/spring/domain/User.java
@@ -1,6 +1,5 @@
 package com.lec.spring.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -61,11 +60,9 @@ public class User {
 
     @OneToMany
     @JoinColumn(name = "userId"
-    , insertable = false, updatable = false)
+            , insertable = false, updatable = false)
     @ToString.Exclude
     private List<Post> posts = new ArrayList<>();
-
-}
 
     // 날짜 자동 저장
     // reliability 자동으로 500 설정
