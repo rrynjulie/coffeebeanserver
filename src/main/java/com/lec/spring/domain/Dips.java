20f4909016987609ffc5365a1551c47465a4ecc4
rrynjulie <rrynjulie@gmail.com>
2024-08-05 11:39:18 +0900

    임예나_20240805_dips_domain

--- src/main/java/com/lec/spring/domain/Dips.java
+++ src/main/java/com/lec/spring/domain/Dips.java
@@ -1,19 +1,35 @@
 package com.lec.spring.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity(name = "dips")
 public class Dips {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long dipsId;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "userId", nullable = false)
+    @ToString.Exclude
+    private User user;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "propertyId", nullable = true)
+    @ToString.Exclude
+    private Property property;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "carId", nullable = true)
+    @ToString.Exclude
+    private Car car;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "productId", nullable = true)
+    @ToString.Exclude
+    private Product product;
 }
