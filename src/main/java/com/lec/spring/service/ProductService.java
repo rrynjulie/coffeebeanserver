2242d5ef01024d579074d163a1cafec5ebd67cb3
eunjeong0911 <eunjeong0911@naver.com>
2024-08-14 12:36:22 +0900

    최은정_20240814_중고물품리스프 API 만들어주기

--- src/main/java/com/lec/spring/service/ProductService.java
+++ src/main/java/com/lec/spring/service/ProductService.java
@@ -77,4 +77,11 @@ public class ProductService {
 
     // 추가 기능
     // TODO
+    public List<Product> getProductsByCategory(String category1, String category2, String category3){
+        return productRepository.findAll().stream()
+                .filter(product -> (category1 == null || product.getCategory1().equals(category1)) &&
+                        (category2 == null || product.getCategory2().equals(category2)) &&
+                        (category3) == null || product.getCategory3().equals(category3))
+                .toList();
+    }
 }
