09f17c802f518a5bbe32fc992492a50f4273d6f3
eunjeong0911 <eunjeong0911@naver.com>
2024-08-18 14:54:34 +0900

    최은정_20240818_카테고리2 평균, 최소, 최대 가격

--- src/main/java/com/lec/spring/service/ProductService.java
+++ src/main/java/com/lec/spring/service/ProductService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
